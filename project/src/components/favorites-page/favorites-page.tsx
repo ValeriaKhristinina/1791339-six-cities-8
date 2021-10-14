218d59f242ee8ae1ac5a47bcb068ab6303a29b69
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-10-14 21:52:24 +0200

    added state for comment form

--- project/src/components/favorites-page/favorites-page.tsx
+++ project/src/components/favorites-page/favorites-page.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import {Offers} from '../../types/offer';
+import { Offers } from '../../types/offer';
 import OffersList from '../offers-list/offers-list';
 // import Card from '../card/card';
 
@@ -7,7 +7,7 @@ type FavoritesPageProps = {
   offers: Offers,
 }
 
-function FavoritesPage({offers}: FavoritesPageProps): JSX.Element {
+function FavoritesPage({ offers }: FavoritesPageProps): JSX.Element {
   return (
     <main className="page__main page__main--favorites">
       <div className="page__favorites-container container">
@@ -18,54 +18,11 @@ function FavoritesPage({offers}: FavoritesPageProps): JSX.Element {
               <div className="favorites__locations locations locations--current">
                 <div className="locations__item">
                   <a className="locations__item-link" href="#">
-                    <span>Amsterdam</span>
+                    <span>{offers[0].city.name}</span>
                   </a>
                 </div>
               </div>
-              <OffersList isFavoritesPage offers={offers}/>
-            </li>
-
-            <li className="favorites__locations-items">
-              <div className="favorites__locations locations locations--current">
-                <div className="locations__item">
-                  <a className="locations__item-link" href="#">
-                    <span>Cologne</span>
-                  </a>
-                </div>
-              </div>
-              <div className="favorites__places">
-                <article className="favorites__card place-card">
-                  <div className="favorites__image-wrapper place-card__image-wrapper">
-                    <a href="#">
-                      <img className="place-card__image" src="img/apartment-small-04.jpg" width="150" height="110" alt="" />
-                    </a>
-                  </div>
-                  <div className="favorites__card-info place-card__info">
-                    <div className="place-card__price-wrapper">
-                      <div className="place-card__price">
-                        <b className="place-card__price-value">&euro;180</b>
-                        <span className="place-card__price-text">&#47;&nbsp;night</span>
-                      </div>
-                      <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-                        <svg className="place-card__bookmark-icon" width="18" height="19">
-                          <use xlinkHref="#icon-bookmark"></use>
-                        </svg>
-                        <span className="visually-hidden">In bookmarks</span>
-                      </button>
-                    </div>
-                    <div className="place-card__rating rating">
-                      <div className="place-card__stars rating__stars">
-                        <span style={{width: '100%'}}></span>
-                        <span className="visually-hidden">Rating</span>
-                      </div>
-                    </div>
-                    <h2 className="place-card__name">
-                      <a href="#">White castle</a>
-                    </h2>
-                    <p className="place-card__type">Apartment</p>
-                  </div>
-                </article>
-              </div>
+              <OffersList isFavoritesPage offers={offers} />
             </li>
           </ul>
         </section>

f6484966d9546cb2a3b87bc7b16ee70f152fe6dc
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-10-21 13:03:11 +0200

    added support Redux DevTools.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './components/app/app';
 import { offers, favoritesOffers } from './mocks/offers';
 import { reducer } from './store/reducer';
@@ -11,7 +12,7 @@ const Setting = {
   RENTS_COUNT: 5,
 };
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>

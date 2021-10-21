00b336ebc64dce8b390c1bd49a8d9b2a97449785
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-10-21 13:01:02 +0200

    added initialization store to project

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { offers, favoritesOffers } from './mocks/offers';
+import { reducer } from './store/reducer';
 
 
 const Setting = {
   RENTS_COUNT: 5,
 };
 
+const store = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      rentsCount={Setting.RENTS_COUNT}
-      offers={offers}
-      favoritesOffers={favoritesOffers}
-    />
+    <Provider store={store}>
+      <App
+        rentsCount={Setting.RENTS_COUNT}
+        offers={offers}
+        favoritesOffers={favoritesOffers}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));

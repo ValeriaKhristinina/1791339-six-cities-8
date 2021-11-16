e3da8e8616e6be697e1c4d760e03deca1d2b567d
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-11-16 11:44:29 +0100

    fixed adapters

--- project/src/services/adapters.ts
+++ project/src/services/adapters.ts
@@ -3,72 +3,87 @@ import {Offer} from '../types/offer';
 import { ReviewType, ServerReviewType } from '../types/review';
 import {ServerOffer} from '../types/server-offer';
 
-export const adaptOfferToClient = (data: ServerOffer): Offer => {
-  const adaptedOffer: any = {
-    ...data,
-    isFavorite: data['is_favorite'],
-    previewImage: data['preview_image'],
-    isPremium: data['is_premium'],
-    maxAdults: data['max_adults'],
-    host: {
-      ...data.host,
-      avatarUrl: data.host['avatar_url'],
-      isPro: data.host['is_pro'],
-    },
-  };
-
-  // Ненужные ключи мы удаляем
-  delete adaptedOffer['preview_image'];
-  delete adaptedOffer['is_premium'];
-  delete adaptedOffer['is_favorite'];
-  delete adaptedOffer['max_adults'];
-  delete adaptedOffer.host['is_pro'];
-  delete adaptedOffer.host['avatar_url'];
-
-  return adaptedOffer;
-};
-
-
-export const adaptOfferToServer = (data: Offer): ServerOffer => {
-  const adaptedOffer: any = {
-    ...data,
-    'is_favorite': data.isFavorite,
-    'preview_image': data.previewImage,
-    'is_premium': data.isPremium,
-    'max_adults': data.maxAdults,
-    host: {
-      ...data.host,
-      'is_pro': data.host.isPro,
-      'avatar_url': data.host.avatarUrl,
-    },
-  };
-
-  // Ненужные ключи мы удаляем
-  delete adaptedOffer.isFavorite;
-  delete adaptedOffer.previewImage;
-  delete adaptedOffer.isPremium;
-  delete adaptedOffer.maxAdults;
-  delete adaptedOffer.host.isPro;
-  delete adaptedOffer.host.avatarUrl;
-
-  return adaptedOffer;
-};
+export const adaptOfferToClient = (data: ServerOffer): Offer => ({
+  bedrooms: data.bedrooms,
+  city: {
+    ...data.city,
+  },
+  description: data.description,
+  goods: data.goods,
+  host: {
+    avatarUrl: data.host['avatar_url'],
+    id: data.host.id,
+    isPro: data.host['is_pro'],
+    name: data.host.name,
+  },
+  id: data.id,
+  images: data.images,
+  isFavorite: data['is_favorite'],
+  isPremium: data['is_premium'],
+  location: {
+    ...data.location,
+  },
+  maxAdults: data['max_adults'],
+  previewImage: data['preview_image'],
+  price: data.price,
+  rating: data.rating,
+  title: data.title,
+  type: data.type,
+});
 
 
-export const adaptCommentsToClient = (data: ServerReviewType): ReviewType => {
-  const adaptedComments: any = {
-    ...data,
-    user: {
-      ...data.user,
-      avatarUrl: data.user['avatar_url'],
-      isPro: data.user['is_pro'],
-    },
-  };
+export const adaptOfferToServer = (data: Offer): ServerOffer => ({
+  bedrooms: data.bedrooms,
+  city: {
+    ...data.city,
+  },
+  description: data.description,
+  goods: data.goods,
+  host: {
+    'avatar_url': data.host.avatarUrl,
+    id: data.host.id,
+    'is_pro': data.host.isPro,
+    name: data.host.name,
+  },
+  id: data.id,
+  images: data.images,
+  'is_favorite': data.isFavorite,
+  'is_premium': data.isPremium,
+  location: {
+    ...data.location,
+  },
+  'max_adults': data.maxAdults,
+  'preview_image': data.previewImage,
+  price: data.price,
+  rating: data.rating,
+  title: data.title,
+  type: data.type,
 
-  // Ненужные ключи мы удаляем
-  delete adaptedComments.user['avatar_url'];
-  delete adaptedComments.user['is_pro'];
+});
 
 
-  return adaptedComments;
-};
+export const adaptCommentsToClient = (data: ServerReviewType): ReviewType => ({
+  comment: data.comment,
+  date: data.date,
+  id: data.id,
+  rating: data.rating,
+  user: {
+    avatarUrl: data.user['avatar_url'],
+    id: data.user.id,
+    isPro: data.user['is_pro'],
+    name: data.user.name,
+  },
+});
+
+export const adaptCommentsToServer = (data: ReviewType): ServerReviewType => ({
+  comment: data.comment,
+  date: data.date,
+  id: data.id,
+  rating: data.rating,
+  user: {
+    'avatar_url': data.user.avatarUrl,
+    id: data.user.id,
+    'is_pro': data.user.isPro,
+    name: data.user.name,
+  },
+});

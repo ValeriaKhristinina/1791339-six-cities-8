e1a9a508b399325f3063621212fa472044af9fd4
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-11-17 16:09:20 +0100

    created fake mocks

---
+++ project/src/utils/mocks.ts
@@ -0,0 +1,54 @@
+import {address, lorem, commerce, name, internet, image, datatype} from 'faker';
+import {Offer} from '../types/offer';
+import { ReviewType } from '../types/review';
+
+const getRandomRaiting = () => (Math.floor(Math.random() * 5) + 1);
+
+export const makeFakeOffer = (): Offer => ({
+  bedrooms: datatype.number(),
+  city: {
+    location: {
+      latitude: Number(address.latitude()),
+      longitude: Number(address.longitude()),
+      zoom: 13,
+    },
+    name: address.cityName(),
+  },
+  description: lorem.paragraph(),
+  goods: new Array(3).fill(null).map(() => (commerce.product())),
+  host: {
+    avatarUrl: internet.avatar(),
+    id: datatype.number(),
+    isPro: datatype.boolean(),
+    name: name.firstName(),
+  },
+  id: datatype.number(),
+  images: new Array(3).fill(null).map(()=>(image.image())),
+  isFavorite: datatype.boolean(),
+  isPremium: datatype.boolean(),
+  location: {
+    latitude: Number(address.latitude()),
+    longitude: Number(address.longitude()),
+    zoom: 13,
+  },
+  maxAdults: datatype.number(),
+  previewImage: image.abstract(),
+  price: datatype.number(),
+  rating: getRandomRaiting(),
+  title: name.title(),
+  type: name.title(),
+} as Offer);
+
+
+export const makeFakeComments = (): ReviewType => ({
+  comment: lorem.paragraph(),
+  date: String(datatype.datetime()),
+  id: datatype.number(),
+  rating: getRandomRaiting(),
+  user: {
+    avatarUrl: internet.avatar(),
+    id: datatype.number(),
+    isPro: datatype.boolean(),
+    name: name.firstName(),
+  },
+});

4a3c89e2338b233305ced5dd6cac437aa565633b
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-11-20 11:35:45 +0100

    created test for hook: useMap

---
+++ project/src/hooks/use-map.test.tsx
@@ -0,0 +1,19 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useMap from './use-map';
+import { CITIES } from '../const';
+
+describe('Hook: useMap', () => {
+
+  it('should return map', () => {
+    const mapRef = { current: document.createElement('div') };
+    const { result } = renderHook(() =>
+
+      useMap(mapRef, CITIES[0]),
+    );
+
+    const map = result.current;
+
+    expect(map).not.toBe(null);
+    expect(map.getCenter().lat).toBe(CITIES[0].location.latitude);
+  });
+});
